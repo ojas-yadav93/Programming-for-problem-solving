e1bf89e7d66aec5665560b639eb32757a073b697
Ojas Yadav <ojas6568@gmail.com>
2026-01-12 10:26:02 +0530

    Add files via upload
    practice problem

---
+++ trianglearea.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float A,b,h;
+    cout<<"The base is: ";
+    cin>>b;
+    cout<<"the height is :";
+    cin>>h;
+    A = 0.5*b*h;
+    cout<<"The area is: "<<A<<endl;
+    return 0;
+}
