165269138884e17b602306cfc4ccb99a0583593b
Ojas Yadav <ojas6568@gmail.com>
2026-01-12 10:33:36 +0530

    Add files via upload
    Area of circle through C++

---
+++ circle area.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float A,r;
+    cout<<"Enter the value of radius"<<endl;
+    cin>>r;
+    A=3.14*r*r;
+    cout<<"The area of circle is"<<A;
+    return 0;
+}
